41d23455c99937597cca7c8fd9ef7c199b043c16
JPE-Stuido <julian@petermaier.at>
2024-08-01 12:12:29 +0200

    adding some animations

--- dist/index.js
+++ dist/index.js
@@ -1,9 +1,49 @@
 
- // This tween will rotate an element with a class of .my-element
- gsap.to('.logo', {
-  rotation: 360,
+ gsap.from('.logo', {
+  scale: 0,
   duration: 2,
   ease: 'bounce.out'
  })
 
-//test
+ gsap.from('.background_blue', {
+    scrollTrigger: '.background_blue', // start the animation when ".box" enters the viewport (once)
+    x: 500,
+    duration: 1,
+     ease: "power1.out"
+});
+
+
+gsap.from('.hero_text_contain', {
+    x: -500,
+    duration: 1,
+    ease: "power1.out"
+});
+
+gsap.from('.hero_image', {
+    y: -500,
+    duration: 1,
+    ease: "power1.out"
+});
+
+
+gsap.from('.background_red', {
+    scrollTrigger: '.background_red',
+    scale: 0,
+    duration: 0.5,
+     ease: "power1.out"
+});
+
+gsap.from('.background_yellow', {
+    scrollTrigger: '.background_yellow',
+    scale: 0,
+    duration: 0.5,
+     ease: "power1.out"
+});
+
+
+gsap.from('.leistungen-background-div', {
+    scrollTrigger: '.leistungen-background-div',
+    x: -1500,
+    duration: 1,
+    ease: "power1.out"
+});
