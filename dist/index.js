7d1ecf292cc0a422b4e0e1d739b919fc9f043ddd
JPE-Stuido <julian@petermaier.at>
2024-08-08 19:02:21 +0200

    Update index.js

--- dist/index.js
+++ dist/index.js
@@ -29,9 +29,15 @@ window.addEventListener("DOMContentLoaded", (event) => {
   }
 
     $("[words-slide-up-start]").each(function (index) {
-      let tl = gsap.timeline({ paused: true });
-      tl.from($(this).find(".word"), { opacity: 0, yPercent: 100, duration: 0.5, ease: "back.out(2)", stagger: { amount: 0.5 } });
-      
+      let tl = gsap.timeline();
+  tl.from($(this).find(".word"), { 
+    opacity: 0, 
+    yPercent: 100, 
+    duration: 0.5, 
+    ease: "back.out(2)", 
+    stagger: { amount: 0.5 } 
+  });
+  tl.play();
     });
 
     $("[words-slide-up]").each(function (index) {
