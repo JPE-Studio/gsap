39590fcb43c81d45f4eedf7615970f37f94b2410
JPE-Stuido <julian@petermaier.at>
2024-08-08 15:04:16 +0200

    Update index.js

--- dist/index.js
+++ dist/index.js
@@ -52,7 +52,7 @@ window.addEventListener("DOMContentLoaded", (event) => {
 
     $("[block-opacity-delay]").each(function (index) {
       let tl = gsap.timeline({ paused: true });
-      tl.to($(this), { opacity: 1, duration: 0.5, ease: "back.out(2)", delay: 0.4 });
+      tl.to($(this), { opacity: 1, duration: 0.5, ease: "back.out(2)", delay: 0.6 });
       createScrollTrigger($(this), tl);
     });
 
